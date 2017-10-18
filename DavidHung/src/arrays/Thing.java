1468b3ecd8a2d44f617648156fe57aaf076ecf71
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-18 15:27:06 -0400

    Updated

---
+++ DavidHung/src/arrays/Thing.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Thing {
+
+	private String description;
+	
+	public Thing() {
+		this.description = description;
+	}
+
+	public String toString() {
+		return " I am a " + description + ". What did you expect?!";
+	}
+	
+}
