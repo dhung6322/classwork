bc2e2ac989a7ad241fd71db989651bac60cbeda3
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-18 15:08:19 -0400

    Updated

--- DavidHung/src/arrays/ObjectMain.java
+++ DavidHung/src/arrays/ObjectMain.java
@@ -3,11 +3,38 @@ package arrays;
 public class ObjectMain {
 
 	public ObjectMain() {
-		// TODO Auto-generated constructor stub
+		Person[] people = new Person[14];
+		populate(people);
+		for(Person p: people) {
+			System.out.println(p);
+		}
+	}
+
+	private void populate(Person[] people) {
+		for(int i = 0; i < people.length; i++) {
+			String firstName = randomNameFrom(Person.FIRST_START,
+					Person.FIRST_MIDDLE, Person.FIRST_END);
+			String lastName = randomNameFrom(Person.LAST_START,
+					Person.LAST_MIDDLE, Person.LAST_END);
+			Borough home = randomBorough();
+			people[i]= new Person(firstName,lastName,home);
+		}
+	}
+
+	private Borough randomBorough() {
+		return Borough.NY_BOROUGHS[(int)(Math.random() * Borough.NY_BOROUGHS.length)];
+	}
+
+	private String randomNameFrom(String[] a, String[] b, String[] c) {
+		return get(a)+get(b)+get(c);
+	}
+
+	private String get(String[] a) {
+		return a[(int)(Math.random() * a.length)];
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ObjectMain obj = new ObjectMain();
 
 	}
 
--- DavidHung/src/arrays/Person.java
+++ DavidHung/src/arrays/Person.java
@@ -1,7 +1,16 @@
 package arrays;
 
 public class Person {
+	
+	public static final String[] FIRST_START = {"Chr", "Am", "L", "D", "Th", "Br", "B"};
+	public static final String[] FIRST_MIDDLE = {"ala", "ima", "e", "o", "anna", "ola", "a"};
+	public static final String[] FIRST_END = {"na", "ck", "n","tt", "rian", "lius", "lion", "les"};
+	
+	public static final String[] LAST_START = {"Bl", "Gr", "Ph", "M", "Thr", "Sh", "Br"};
+	public static final String[] LAST_MIDDLE = {"an", "in", "ast", "own", "il"};
+	public static final String[] LAST_END = {"strom", "son", "rack","les", "vin", "ston"};
 	private String firstName;
+	
 	private String lastName;
 	private Borough home;
 	
