9613026a65fc3994faf0b7cd51e5ec400404c3d5
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-01 15:01:43 -0400

    Updated

--- DavidHung/src/caveExplorer/NPC.java
+++ DavidHung/src/caveExplorer/NPC.java
@@ -2,8 +2,31 @@ package caveExplorer;
 
 public class NPC {
 
+	//fields needed to program navigation
+	private CaveRoom[][] floor; //area where NPC roams
+	private int currentRow;
+	private int currentCol;
+	private NPCRoom currentRoom;
+	
+	//these fields are about interaction with NPC
+	private boolean active;
+	private String activeDescription;
+	private String inactiveDescription;
+	//you might later add fields to make your NPC behave like a chatbot
+	
+	//default
 	public NPC() {
-		// TODO Auto-generated constructor stub
+		this.floor = CaveExplorer.caves;
+		this.activeDescription = "There is a person standing in the room "
+				+ "waiting to talk to you. Press 'e' to talk.";
+		this.inactiveDescription = "The person you spoke to earlier is "
+				+ "standing here.";
+		//by default, NPC does not have a position,
+		//to indicate this, use coordinates -1, -1
+		this.currentCol = -1;
+		this.currentRow = -1;
+		currentRoom = null;
+		active = true;
 	}
 
 	public boolean isActive() {
