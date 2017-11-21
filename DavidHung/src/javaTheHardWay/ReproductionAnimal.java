c80c0b222ca52b49473fb304b4188f3647326b0a
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-21 14:14:03 -0500

    Updated

--- DavidHung/src/javaTheHardWay/ReproductionAnimal.java
+++ DavidHung/src/javaTheHardWay/ReproductionAnimal.java
@@ -1,20 +1,64 @@
 package javaTheHardWay;
 
 public class ReproductionAnimal extends RoamingAnimal {
+	
+	private int maxLitter;
 
+
+	public static void main(String[] args) {
+		Habitat h = new Habitat(3);
+		ReproductionAnimal animal1 = new ReproductionAnimal(h);
+		ReproductionAnimal animal2 = new ReproductionAnimal(h);
+		ReproductionAnimal youngAnimal = animal1.getOffspring(animal2);
+		while(youngAnimal == null) {
+			animal2 = new ReproductionAnimal(h);
+			youngAnimal = animal1.getOffspring(animal2);
+		}
+	}
+	
 	public ReproductionAnimal(Habitat matingArea) {
 		super(matingArea);
-		// TODO Auto-generated constructor stub
+		maxLitter = 1;
 	}
 
 	public ReproductionAnimal(Habitat matingArea, String description, Trait trait1, Trait trait2) {
 		super(matingArea, description, trait1, trait2);
-		// TODO Auto-generated constructor stub
+		maxLitter = 1;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public void setMaxLitterSize(int max) {
+		this.maxLitter = max;
 	}
-
+	
+	public ReproductionAnimal getOffspring(ReproductionAnimal mate) {
+				return new ReproductionAnimal(habitat, getDescription(),
+						Trait.getDominantTrait(getTrait1(),mate.getTrait2()),
+						Trait.getDominantTrait(getTrait2(), mate.getTrait2()));
+	}
+	
+	public final void mate() {
+		
+		int i = 0;
+		while(i < habitat.getAnimals().length) {
+			Animal target = habitat.getAnimals()[i];
+			if(target instanceof ReproductionAnimal &&
+					target.getDescription().equals(getDescription()) &&
+					((ReproductionAnimal) target).getSex()!= getSex() &&
+					!target.hasMated()) {
+				Animal baby = getOffspring((ReproductionAnimal)target);
+				if(baby != null) {
+					int litterSize = (int)(Math.random() * maxLitter);
+					for(int j = 1; j < litterSize; j++) {
+						habitat.addAnimal(baby);
+						baby = getOffspring((ReproductionAnimal)target);
+					}
+					setMated(true);
+					target.setMated(true);
+					break;
+				}
+			}
+			i++;
+		}
+	}
+	
 }
