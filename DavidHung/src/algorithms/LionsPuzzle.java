8372e4804dfe8ccee55c7fe48cdba6566f9b72ce
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-30 15:20:57 -0500

    Updated

--- DavidHung/src/algorithms/LionsPuzzle.java
+++ DavidHung/src/algorithms/LionsPuzzle.java
@@ -3,8 +3,24 @@ package algorithms;
 public class LionsPuzzle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numberOfLions = 101;
+		if(willEat(numberOfLions)) {
+			System.out.println("Eat the sheep!");
+		}
+		else {
+			System.out.println("Do not eat the sheep!");
+		}
+	}
 
+	private static boolean willEat(int numberOfLions) {
+		//base case is one lion and should ALWAYS be TRIVIAL
+		if(numberOfLions == 1) {
+			return true;
+		}
+		else {
+			return !willEat(numberOfLions - 1);
+		}
 	}
 
 }
+	
