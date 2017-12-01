a0f5e242f282779bec3fda6ea766c5c8dff30454
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-12-01 14:30:53 -0500

    Updated

---
+++ DavidHung/src/algorithms/RecursionExercises.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class RecursionExercises {
+	
+	public static void main(String[] args){
+		 int value = 5;
+		 System.out.println(value+"! is equal to "+factorial(value));
+		 System.out.println(value+ " has "+countPrimeFactors(value, 2)+" prime factors.");
+	}
+
+	public static int countPrimeFactors(int value, int testPrime) {
+		if(value == testPrime) {
+			return 1;
+		}else if(value % testPrime == 0) {
+			return countPrimeFactors(value, testPrime + 1);
+		}
+		else {
+			return countPrimeFactors(value, testPrime + 1);
+		}
+	}
+
+	public static int factorial(int value) {
+		if(value > 1) {
+			return value * (factorial(value-1));
+		}
+		else {
+			return 1;
+		}
+	}
+
+}
